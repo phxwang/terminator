4ded54376a95f9555136d4ba9b2a85164b2e3955
phx wang <phx.wang@gmail.com>
2025-07-18 16:38:06 +0000

    Add preload_swap_instructions call in liquidate_with_loaded_data for obligation refresh

--- terminator/src/liquidation_engine.rs
+++ terminator/src/liquidation_engine.rs
@@ -765,6 +765,8 @@ pub async fn liquidate_with_loaded_data(
         let new_obligation = klend_client.fetch_obligation(obligation_key).await?;
         *ob = new_obligation;
 
+        self.preload_swap_instructions(klend_client, obligation_key, ob).await?;
+
         info!("Liquidating: Refreshing obligation in obligation_map {:?}, {:?}", obligation_key, self.obligation_map.get(obligation_key).unwrap().to_string());
         
         Ok(())
@@ -1102,7 +1104,7 @@ pub async fn liquidate_with_loaded_data(
         self.scan_obligations(klend_client, liquidatable_obligations, &reserves, &lending_market, &rts, None).await;
     }
 
-    pub async fn preload_swap_instructions(&mut self, klend_client: &Arc<KlendClient>, obligation_key: &Pubkey, obligation: &Obligation) -> Result<()> {
+pub async fn preload_swap_instructions(&mut self, klend_client: &Arc<KlendClient>, obligation_key: &Pubkey, obligation: &Obligation) -> Result<()> {
         let debt_res_key = match math::find_best_debt_reserve(&obligation.borrows, &self.all_reserves) {
             Some(key) => key,
             None => {
